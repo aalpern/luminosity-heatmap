472a88e769f3c26d19dfe7dc3cfce6db21a087f7
Adam Alpern <adam.alpern@gmail.com>
2016-03-25 08:15:22 -0700

    Add exposure time to the sunburst chart

--- one-catalog/app.js
+++ one-catalog/app.js
@@ -1,10 +1,10 @@
-function transmogrify() {
+function transmogrify(catalog) {
   var camera_groups = _.groupBy(catalog.photos, function(p) { return p.camera })
 
   camera_groups = Object.keys(camera_groups).map(function(camera) {
     var photos      = camera_groups[camera]
-    var lens_groups = _.groupBy(photos, function(p) { return p.lens })
 
+    var lens_groups = _.groupBy(photos, function(p) { return p.lens })
     lens_groups = Object.keys(lens_groups).map(function(lens) {
       var photos = lens_groups[lens]
 
@@ -12,9 +12,19 @@ function transmogrify() {
       fnumber_groups = Object.keys(fnumber_groups).map(function(fnumber) {
         var photos = fnumber_groups[fnumber]
 
+        var exposure_time_groups = _.groupBy(photos, function(p) { return p.exposure_time })
+        exposure_time_groups = Object.keys(exposure_time_groups).map(function(exposure) {
+          var photos = exposure_time_groups[exposure]
+
+          return {
+            name: exposure,
+            size: photos.length
+          }
+        })
+
         return {
           name: "f/" + fnumber,
-          size: photos.length
+          children: exposure_time_groups
         }
       })
 
@@ -33,7 +43,7 @@ function transmogrify() {
   return [{
     name: "All",
     children: camera_groups
-}]
+  }]
 }
 
 
